f1f7a3ce9f6e5f41d690e1485ae34f2d4573f80e
Zachary Guidry <zachguidry58@gmail.com>
2025-07-13 17:15:21 -0500

    Spots Stage 5: Form Submission

--- scripts/index.js
+++ scripts/index.js
@@ -20,4 +20,41 @@ newPostBtn.addEventListener("click", function () {
 
 newPostCloseBtn.addEventListener("click", function () {
   newPostModal.classList.remove("modal_is-opened");
+  newPostForm.reset();
 });
+
+const profileModal = document.querySelector(".profile");
+const profileName = profileModal.querySelector(".profile__name");
+const profileDescription = profileModal.querySelector(".profile__description");
+
+const editProfileForm = editProfileModal.querySelector(".form");
+const editProfileName = editProfileForm.querySelector("#name");
+const editProfileDescription = editProfileForm.querySelector("#description");
+editProfileName.value = profileName.textContent;
+editProfileDescription.value = profileDescription.textContent;
+//form submission handler
+function handleProfileFormSubmit(event) {
+  event.preventDefault();
+  let newProfileName = editProfileName.value;
+  let newProfileDescription = editProfileDescription.value;
+  profileName.textContent = newProfileName;
+  profileDescription.textContent = newProfileDescription;
+
+  editProfileModal.classList.remove("modal_is-opened");
+}
+//event listner for edit profile form
+editProfileForm.addEventListener("submit", handleProfileFormSubmit);
+
+const newPostForm = newPostModal.querySelector(".form");
+const newPostNameInput = newPostForm.querySelector("#caption");
+const newPostImageInput = newPostForm.querySelector("#link");
+//new post submission handler
+function handleAddCardSubmit(event) {
+  event.preventDefault();
+  console.log(newPostNameInput.value);
+  console.log(newPostImageInput.value);
+  newPostModal.classList.remove("modal_is-opened");
+  event.target.reset();
+}
+//event listener for new post form
+newPostForm.addEventListener("submit", handleAddCardSubmit);
