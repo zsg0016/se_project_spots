5ed084d09c177f9230f0db1cbe4dee406e72e9d8
Zachary Guidry <zachguidry58@gmail.com>
2025-07-30 06:57:26 -0500

    Spots Stage 8: Generating Cards Dynamically

--- scripts/index.js
+++ scripts/index.js
@@ -33,6 +33,52 @@ const newPostBtn = document.querySelector(".profile__new-post-btn");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPostModal.querySelector(".form__close-btn");
 
+const cardTemplate = document.querySelector("#card-template");
+const cardList = document.querySelector(".cards__list");
+
+const previewPostModal = document.querySelector("#preview-post-modal");
+const previewPostCloseBtn = previewPostModal.querySelector(
+  ".preview__close-btn"
+);
+
+previewPostCloseBtn.addEventListener("click", function (evt) {
+  closeModal(previewPostModal);
+});
+
+const previewPostImage = previewPostModal.querySelector(".preview__image");
+const previewPostTitle = previewPostModal.querySelector(".preview__caption");
+
+function getCardElement(data) {
+  const cardElement = cardTemplate.content
+    .querySelector(".card")
+    .cloneNode(true);
+  const cardTitle = cardElement.querySelector(".card__caption");
+  const cardImage = cardElement.querySelector(".card__image");
+  cardTitle.textContent = data.name;
+  cardImage.setAttribute("src", data.link);
+  cardImage.setAttribute("alt", data.name);
+  const cardLikeBtn = cardElement.querySelector(".card__like");
+  cardLikeBtn.setAttribute("style", "cursor: pointer");
+
+  cardImage.addEventListener("click", function (evt) {
+    previewPostImage.setAttribute("src", data.link);
+    previewPostImage.setAttribute("alt", data.name);
+    previewPostTitle.textContent = data.name;
+    openModal(previewPostModal);
+  });
+
+  cardLikeBtn.addEventListener("click", function (evt) {
+    evt.target.classList.toggle("card__like-clicked");
+    evt.target.classList.toggle("card__like");
+  });
+
+  const cardDeleteBtn = cardElement.querySelector(".card__delete-btn");
+  cardDeleteBtn.addEventListener("click", function (evt) {
+    cardElement.remove();
+  });
+  return cardElement;
+}
+
 function openModal(modal) {
   modal.classList.add("modal_is-opened");
 }
@@ -85,13 +131,18 @@ const newPostImageInput = newPostForm.querySelector("#link");
 //new post submission handler
 function handleAddCardSubmit(event) {
   event.preventDefault();
-  console.log(newPostNameInput.value);
-  console.log(newPostImageInput.value);
+  initialCards.push({
+    name: newPostNameInput.value,
+    link: newPostImageInput.value,
+  });
+  const newCard = getCardElement(initialCards[initialCards.length - 1]);
+  cardList.prepend(newCard);
   closeModal(newPostModal);
   event.target.reset();
 }
 //event listener for new post form
 newPostForm.addEventListener("submit", handleAddCardSubmit);
-initialCards.forEach(function (item) {
-  console.log(item.name);
+initialCards.forEach(function (data) {
+  const newCard = getCardElement(data);
+  cardList.prepend(newCard);
 });
